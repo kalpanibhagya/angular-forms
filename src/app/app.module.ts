87c11c4f572f8acfdcb5344d50531ca3b5d3df15
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2018-05-07 22:20:13 +0530

    Add form1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { Form1Component } from './components/form1/form1.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Form1Component
   ],
   imports: [
     BrowserModule
